22bb91e0e87cda66bbc1fe57717b4011c9586d41
Miguel Campos <camposmiguel@gmail.com>
2021-07-28 10:01:42 +0200

    interfaces

---
+++ angular-clase-dia3/src/app/component-hijo/component-hijo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-hijo',
+  templateUrl: './component-hijo.component.html',
+  styleUrls: ['./component-hijo.component.css']
+})
+export class ComponentHijoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-clase-dia3/src/app/component-padre/component-padre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-component-padre',
+  templateUrl: './component-padre.component.html',
+  styleUrls: ['./component-padre.component.css']
+})
+export class ComponentPadreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
